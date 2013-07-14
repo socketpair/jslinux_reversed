172aaaf948216f7fabfb7a5b10b87ac03d3ded59
Коренберг Марк (дома) <socketpair@gmail.com>
2013-07-15 04:46:01 +0600

    code de-duplicate

--- clipboard.js
+++ clipboard.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function ClipboardDevice_std(Pg, ag, th, nh, uh) {
+function ClipboardDevice(Pg, ag, th, nh, uh) {
     Pg.register_ioport_read(ag, 16, 4, this.ioport_readl.bind(this));
     Pg.register_ioport_write(ag, 16, 4, this.ioport_writel.bind(this));
     Pg.register_ioport_read(ag + 8, 1, 1, this.ioport_readb.bind(this));
@@ -11,10 +11,10 @@ function ClipboardDevice_std(Pg, ag, th, nh, uh) {
     this.write_func = nh;
     this.get_boot_time = uh;
 }
-ClipboardDevice_std.prototype.ioport_writeb = function (ia, ja) {
+ClipboardDevice.prototype.ioport_writeb = function (ia, ja) {
     this.doc_str += String.fromCharCode(ja);
 };
-ClipboardDevice_std.prototype.ioport_readb = function (ia) {
+ClipboardDevice.prototype.ioport_readb = function (ia) {
     var c, qa, ja;
     qa = this.doc_str;
     if (this.cur_pos < qa.length) {
@@ -25,7 +25,7 @@ ClipboardDevice_std.prototype.ioport_readb = function (ia) {
     this.cur_pos++;
     return ja;
 };
-ClipboardDevice_std.prototype.ioport_writel = function (ia, ja) {
+ClipboardDevice.prototype.ioport_writel = function (ia, ja) {
     var qa;
     ia = (ia >> 2) & 3;
     switch (ia) {
@@ -40,9 +40,10 @@ ClipboardDevice_std.prototype.ioport_writel = function (ia, ja) {
             break;
         case 3:
             this.write_func(this.doc_str);
+            break;
     }
 };
-ClipboardDevice_std.prototype.ioport_readl = function (ia) {
+ClipboardDevice.prototype.ioport_readl = function (ia) {
     var ja;
     ia = (ia >> 2) & 3;
     switch (ia) {
@@ -58,6 +59,10 @@ ClipboardDevice_std.prototype.ioport_readl = function (ia) {
             ja |= this.ioport_readb(0) << 24;
             return ja;
         case 3:
-            if (this.get_boot_time)return this.get_boot_time() >> 0; else return 0;
+            if (this.get_boot_time) {
+                return this.get_boot_time() >> 0;
+            } else {
+                return 0;
+            }
     }
 };
--- pic.js
+++ pic.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function PIC_sub_std(Pg, ag) {
+function PIC_sub(Pg, ag) {
     Pg.register_ioport_write(ag, 2, 1, this.ioport_write.bind(this));
     Pg.register_ioport_read(ag, 2, 1, this.ioport_read.bind(this));
     this.reset();
 }
-PIC_sub_std.prototype.reset = function () {
+PIC_sub.prototype.reset = function () {
     this.last_irr = 0;
     this.irr = 0;
     this.imr = 0;
@@ -21,28 +21,36 @@ PIC_sub_std.prototype.reset = function () {
     this.elcr = 0;
     this.elcr_mask = 0;
 };
-PIC_sub_std.prototype.set_irq1 = function (Tg, Rf) {
+PIC_sub.prototype.set_irq1 = function (Tg, Rf) {
     var xc;
     xc = 1 << Tg;
     if (Rf) {
-        if ((this.last_irr & xc) == 0)this.irr |= xc;
+        if ((this.last_irr & xc) == 0) {
+            this.irr |= xc;
+        }
         this.last_irr |= xc;
     } else {
         this.last_irr &= ~xc;
     }
 };
-PIC_sub_std.prototype.get_priority = function (xc) {
+PIC_sub.prototype.get_priority = function (xc) {
     var Ug;
-    if (xc == 0)return-1;
+    if (xc == 0) {
+        return-1;
+    }
     Ug = 7;
-    while ((xc & (1 << ((Ug + this.priority_add) & 7))) == 0)Ug--;
+    while ((xc & (1 << ((Ug + this.priority_add) & 7))) == 0) {
+        Ug--;
+    }
     return Ug;
 };
-PIC_sub_std.prototype.get_irq = function () {
+PIC_sub.prototype.get_irq = function () {
     var xc, Vg, Ug;
     xc = this.irr & ~this.imr;
     Ug = this.get_priority(xc);
-    if (Ug < 0)return-1;
+    if (Ug < 0) {
+        return-1;
+    }
     Vg = this.get_priority(this.isr);
     if (Ug > Vg) {
         return Ug;
@@ -50,15 +58,19 @@ PIC_sub_std.prototype.get_irq = function () {
         return-1;
     }
 };
-PIC_sub_std.prototype.intack = function (Tg) {
+PIC_sub.prototype.intack = function (Tg) {
     if (this.auto_eoi) {
-        if (this.rotate_on_auto_eoi)this.priority_add = (Tg + 1) & 7;
+        if (this.rotate_on_auto_eoi) {
+            this.priority_add = (Tg + 1) & 7;
+        }
     } else {
         this.isr |= (1 << Tg);
     }
-    if (!(this.elcr & (1 << Tg)))this.irr &= ~(1 << Tg);
+    if (!(this.elcr & (1 << Tg))) {
+        this.irr &= ~(1 << Tg);
+    }
 };
-PIC_sub_std.prototype.ioport_write = function (ia, ja) {
+PIC_sub.prototype.ioport_write = function (ia, ja) {
     var Ug;
     ia &= 1;
     if (ia == 0) {
@@ -66,11 +78,19 @@ PIC_sub_std.prototype.ioport_write = function (ia, ja) {
             this.reset();
             this.init_state = 1;
             this.init4 = ja & 1;
-            if (ja & 0x02)throw"single mode not supported";
-            if (ja & 0x08)throw"level sensitive irq not supported";
+            if (ja & 0x02) {
+                throw"single mode not supported";
+            }
+            if (ja & 0x08) {
+                throw"level sensitive irq not supported";
+            }
         } else if (ja & 0x08) {
-            if (ja & 0x02)this.read_reg_select = ja & 1;
-            if (ja & 0x40)this.special_mask = (ja >> 5) & 1;
+            if (ja & 0x02) {
+                this.read_reg_select = ja & 1;
+            }
+            if (ja & 0x40) {
+                this.special_mask = (ja >> 5) & 1;
+            }
         } else {
             switch (ja) {
                 case 0x00:
@@ -83,7 +103,9 @@ PIC_sub_std.prototype.ioport_write = function (ia, ja) {
                     if (Ug >= 0) {
                         this.isr &= ~(1 << ((Ug + this.priority_add) & 7));
                     }
-                    if (ja == 0xa0)this.priority_add = (this.priority_add + 1) & 7;
+                    if (ja == 0xa0) {
+                        this.priority_add = (this.priority_add + 1) & 7;
+                    }
                     break;
                 case 0x60:
                 case 0x61:
@@ -144,20 +166,24 @@ PIC_sub_std.prototype.ioport_write = function (ia, ja) {
         }
     }
 };
-PIC_sub_std.prototype.ioport_read = function (Wg) {
+PIC_sub.prototype.ioport_read = function (Wg) {
     var ia, Rg;
     ia = Wg & 1;
     if (ia == 0) {
-        if (this.read_reg_select)Rg = this.isr; else Rg = this.irr;
+        if (this.read_reg_select) {
+            Rg = this.isr;
+        } else {
+            Rg = this.irr;
+        }
     } else {
         Rg = this.imr;
     }
     return Rg;
 };
-function PIC_std(Pg, Yg, Wg, Zg) {
-    this.pics = new Array();
-    this.pics[0] = new PIC_sub_std(Pg, Yg);
-    this.pics[1] = new PIC_sub_std(Pg, Wg);
+function PIC(Pg, Yg, Wg, Zg) {
+    this.pics = [];
+    this.pics[0] = new PIC_sub(Pg, Yg);
+    this.pics[1] = new PIC_sub(Pg, Wg);
     this.pics[0].elcr_mask = 0xf8;
     this.pics[1].elcr_mask = 0xde;
     this.irq_requested = 0;
@@ -165,7 +191,7 @@ function PIC_std(Pg, Yg, Wg, Zg) {
     this.pics[0].update_irq = this.update_irq.bind(this);
     this.pics[1].update_irq = this.update_irq.bind(this);
 }
-PIC_std.prototype.update_irq = function () {
+PIC.prototype.update_irq = function () {
     var ah, Tg;
     ah = this.pics[1].get_irq();
     if (ah >= 0) {
@@ -179,11 +205,11 @@ PIC_std.prototype.update_irq = function () {
         this.cpu_set_irq(0);
     }
 };
-PIC_std.prototype.set_irq = function (Tg, Rf) {
+PIC.prototype.set_irq = function (Tg, Rf) {
     this.pics[Tg >> 3].set_irq1(Tg & 7, Rf);
     this.update_irq();
 };
-PIC_std.prototype.get_hard_intno = function () {
+PIC.prototype.get_hard_intno = function () {
     var Tg, ah, intno;
     Tg = this.pics[0].get_irq();
     if (Tg >= 0) {
