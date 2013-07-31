4b572b2948fd082ba018127811488ddefc0519c3
Коренберг Марк <mark@ideco.ru>
2013-07-31 15:51:21 +0600

    utf8 support

--- clipboard.js
+++ clipboard.js
@@ -1,5 +1,67 @@
 "use strict";
 
+function jslinux_str2utf8(string) {
+    var utftext = "";
+    var n;
+    var c;
+
+    for (n = 0; n < string.length; n++) {
+
+        c = string.charCodeAt(n);
+
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        }
+        else if ((c > 127) && (c < 2048)) {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+        else {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+
+    }
+
+    return utftext;
+}
+
+
+function jslinux_utf82str(utftext) {
+    var string = "";
+    var i = 0;
+    var c = 0;
+    var c1 = 0;
+    var c2 = 0;
+    var c3 = 0;
+
+    while (i < utftext.length) {
+
+        c = utftext.charCodeAt(i);
+
+        if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+        }
+        else if ((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i + 1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        }
+        else {
+            c2 = utftext.charCodeAt(i + 1);
+            c3 = utftext.charCodeAt(i + 2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+
+    }
+
+    return string;
+}
+
+
 function ClipboardDevice(pc_emulator, io_port, read_func, write_func, get_boot_time) {
     pc_emulator.register_ioport_read(io_port, 16, 4, this.ioport_readl.bind(this));
     pc_emulator.register_ioport_write(io_port, 16, 4, this.ioport_writel.bind(this));
@@ -47,7 +109,7 @@ ClipboardDevice.prototype.ioport_writel = function (io_port, dword_value) {
             this.doc_str += text;
             break;
         case 3:
-            this.write_func(this.doc_str);
+            this.write_func(jslinux_utf82str(this.doc_str));
             break;
     }
 };
@@ -56,7 +118,7 @@ ClipboardDevice.prototype.ioport_readl = function (io_port) {
     io_port = (io_port >> 2) & 3;
     switch (io_port) {
         case 0:
-            this.doc_str = this.read_func();
+            this.doc_str = jslinux_str2utf8(this.read_func());
             return this.doc_str.length >> 0;
         case 1:
             return this.cur_pos >> 0;
--- term.js
+++ term.js
@@ -7,6 +7,68 @@
  permission.
  */
 "use strict";
+
+
+function jslinux_str2utf8(string) {
+    var utftext = "";
+    var n;
+    var c;
+
+    for (n = 0; n < string.length; n++) {
+
+        c = string.charCodeAt(n);
+
+        if (c < 128) {
+            utftext += String.fromCharCode(c);
+        }
+        else if ((c > 127) && (c < 2048)) {
+            utftext += String.fromCharCode((c >> 6) | 192);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+        else {
+            utftext += String.fromCharCode((c >> 12) | 224);
+            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+            utftext += String.fromCharCode((c & 63) | 128);
+        }
+
+    }
+
+    return utftext;
+}
+
+
+function jslinux_utf82str(utftext) {
+    var string = "";
+    var i = 0;
+    var c = 0;
+    var c1 = 0;
+    var c2 = 0;
+    var c3 = 0;
+
+    while (i < utftext.length) {
+
+        c = utftext.charCodeAt(i);
+
+        if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+        }
+        else if ((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i + 1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        }
+        else {
+            c2 = utftext.charCodeAt(i + 1);
+            c3 = utftext.charCodeAt(i + 2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+
+    }
+    return string;
+}
+
 function Term(width, height, handler) {
     this.w = width;
     this.h = height;
@@ -199,7 +261,7 @@ Term.prototype.refresh = function (fa, ga) {
         if (def_attr != this.def_attr) {
             inner_html += '</span>';
         }
-        this.tlines[y].innerHTML = inner_html;
+        this.tlines[y].innerHTML = jslinux_utf82str(inner_html);
     }
 };
 Term.prototype.cursor_timer_cb = function () {
